428e3e7b33b14d17e1284087c9c3b659475b0a26
DanielWist <vistdaniel277@gmail.com>
2025-04-01 13:28:11 +0300

    add pupsiki

--- app/src/main/java/com/example/explorejapanapp/MainActivity.kt
+++ app/src/main/java/com/example/explorejapanapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ExploreJapanAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Bebriki and Shuliki",
+                        name = "Bebriki and Shuliki and Pupsiki",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ExploreJapanAppTheme {
-        Greeting("Bebriki and Shuliki")
+        Greeting("Bebriki and Shuliki and Pupsiki")
     }
 }
