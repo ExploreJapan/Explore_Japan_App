caf8e181debcfe2e4d41f4f9e0f5ab0a10f47c10
PinKir1 <kiril_kiryak@ukr.net>
2025-04-09 01:04:08 +0300

    добавил шапку и подвал

--- app/src/main/java/com/example/explorejapanapp/MainActivity.kt
+++ app/src/main/java/com/example/explorejapanapp/MainActivity.kt
@@ -4,13 +4,5 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val db = DatabaseProvider.getDatabase(this)
 
-        setContent {
-            HomeScreen(homeDao = db.homeDao())
-        }
-    }
-}
+
