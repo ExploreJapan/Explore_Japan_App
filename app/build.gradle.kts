709b82fb668984365a4eb75faa4939c6e964f917
PinKir1 <kiril_kiryak@ukr.net>
2025-04-09 02:22:22 +0300

    Сделал макет главной страницы, подключил ее к мейнактивити и сделал шапку с подвалом

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id("kotlin-kapt")
     id("androidx.room") version "2.6.1" apply true
+    id("kotlin-android")
 }
 
 android {
@@ -64,5 +65,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
--- app/src/main/java/com/example/explorejapanapp/MainActivity.kt
+++ app/src/main/java/com/example/explorejapanapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.explorejapanapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 
-
-
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_page)
+    }
+}
