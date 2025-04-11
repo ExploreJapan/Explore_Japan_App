ae5aad42dd1618990a8c544f965087b405e82532
PinKir1 <kiril_kiryak@ukr.net>
2025-04-12 00:08:32 +0300

    поменял версию БД на 1

--- app/src/main/java/com/example/explorejapanapp/AppDatabase.kt
+++ app/src/main/java/com/example/explorejapanapp/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.explorejapanapp
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [HomeContent::class, Profile::class], version = 2, exportSchema = true)
+@Database(entities = [HomeContent::class, Profile::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun homeDao(): HomeDao
     abstract fun profileDao(): ProfileDao
