078d72d1fb27f44837c3eec1b647f7d5a4a6c175
PinKir1 <kiril_kiryak@ukr.net>
2025-04-21 19:39:46 +0300

    сделал стиль для страницы профиля

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,5 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.fragment:fragment-ktx:1.7.1")
+    implementation ("com.google.android.material:material:1.9.0")
 }
